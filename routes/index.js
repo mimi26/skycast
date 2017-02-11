ebf8a4026b0dabbca4753362c7a3b11e62073831
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-02-11 15:01:00 -0500

    hide dark sky api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+require('dotenv').config();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  axios.get(`https://api.darksky.net/forecast/${process.env.SECRET_KEY}/40.730610,-73.935242`)
+  .then((response) => {
+    console.log("this is temp:", response.data.currently.temperature);
+    const temp = Math.round(response.data.currently.temperature);
+    res.render('index', { temp });
+  });
 });
 
 module.exports = router;
+
+
