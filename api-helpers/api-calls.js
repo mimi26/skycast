d142e9540a596f3dfd64f6b7e51c6a90b131144e
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-02-13 13:14:40 -0500

    reconfigured api calls with helper functions

--- api-helpers/api-calls.js
+++ api-helpers/api-calls.js
@@ -1,42 +1,34 @@
 const axios = require('axios');
 const models = require('../db/models/index');
-
 require('dotenv').config();
 
-// router.put('/:id', function(req, res, next) {
-//   models.Technology.update({
-//     name: req.body.name,
-//     description: req.body.description,
-//     documentation: req.body.documentation,
-//     use_case: req.body.use_case,
-//     category_name: req.body.category_name
-//   }, { where: { id: req.params.id} }).then(() => {
-//     res.redirect(`/technologies/${req.params.id}`);
-//   });
-// });
 
-function apiCalls (req, res, next) {
-  models.Address.findAll( {
-    where: { id: req.params.id}
-  }).then((data) => {
-    const address = data;
 
-  //console.log('this is addres:', address);
-    });
-  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=${process.env.MAP_KEY}`)
+function geocodeCall (req, res, next) {
+
+  const address = req.body.address;
+  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.MAP_KEY}`)
   .then((info) => {
-    const lat = info.data.results[0].geometry.location.lat;
-    const lng = info.data.results[0].geometry.location.lng;
-    //res.locals.location = location;
-  axios.get(`https://api.darksky.net/forecast/${process.env.WEATHER_KEY}/${lat},${lng}`)
-  .then((response) => {
+    res.locals.lat = info.data.results[0].geometry.location.lat;
+    res.locals.lng = info.data.results[0].geometry.location.lng;
+    res.locals.city = info.data.results[0].address_components[3].long_name;
+    res.locals.neighborhood = info.data.results[0].address_components[2].long_name;
 
-    res.locals.response = response;
     return next();
-    });
   });
 }
 
+function darkSkyCall (req, res, next) {
+  axios.get(`https://api.darksky.net/forecast/${process.env.WEATHER_KEY}/${res.locals.lat},${res.locals.lng}`)
+  .then((weather) => {
+      res.locals.temp = Math.round(weather.data.currently.temperature);
+      res.locals.summary = weather.data.currently.summary;
+      return next();
+     });
+}
 
 
-module.exports = { apiCalls };
+module.exports = {
+  geocodeCall,
+  darkSkyCall
+ };
--- routes/index.js
+++ routes/index.js
@@ -2,35 +2,20 @@ var express = require('express');
 var router = express.Router();
 const models = require('../db/models/index');
 const axios = require('axios');
-//const apiCall = require('../api-helpers/api-calls');
+const apiCall = require('../api-helpers/api-calls');
 require('dotenv').config();
-
 
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.post('/', function(req, res, next) {
-  const address = req.body.address.replace(/ /g, "+");
-  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.MAP_KEY}`)
-  .then((info) => {
-    const lat = info.data.results[0].geometry.location.lat;
-    const lng = info.data.results[0].geometry.location.lng;
-    return axios.get(`https://api.darksky.net/forecast/${process.env.WEATHER_KEY}/${lat},${lng}`)
-  })
-  .then((weather) => {
-      const temp = Math.round(weather.data.currently.temperature);
-      const summary = weather.data.currently.summary;
-      const city = weather.data.timezone.replace(/^[^/]*\//g, "").replace(/_/g, " ");
-      console.log('this is city:', city);
-
-    res.render('index', { temp, summary });
-     });
-  });
-
-
-
-
+router.post('/', apiCall.geocodeCall, apiCall.darkSkyCall, function(req, res, next) {
+  const city = res.locals.city;
+  const neighborhood = res.locals.neighborhood;
+  const temp = res.locals.temp;
+  const summary = res.locals.summary;
+  res.render('index', { city, neighborhood, temp, summary });
+});
 
 module.exports = router;
 
